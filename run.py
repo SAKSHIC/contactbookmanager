e6095d5c57d49547347b243e26cd56493d4398f5
SAKSHIC <chourasiasakshi@gmail.com>
2018-04-09 01:48:46 +0530

    Update run.py

--- run.py
+++ run.py
@@ -125,4 +125,5 @@ def main():
             sform = sform, data = data, reset = reset)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
